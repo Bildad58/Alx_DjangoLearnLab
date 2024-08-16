c35bf8795f9f92502f6647212bf46c73572ae40d
Bill . <bildadwasioya@gmail.com>
2024-08-16 11:00:49 +0300

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Book
 from .models import Library
-from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+# from django.http import HttpResponse
 
-def book_list(request):
+def list_books(request):
+      """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
-      return render(request, 'relationship_app/book_list.html', context)
-# def list_books(request):
-#     book = Book.objects.all()
-#     context = {
-#         'book': book
-#     }
-
-#     return render(request, 'relationship_app/list_books.html', context)
-
-
+      return render(request, 'relationship_app/list_books.html', context)
+      # return HttpResponse(book_list, content_type="text/plain")
+  
+# class DetailView(DetailView):
+#   """A class-based view for displaying details of a specific book."""
+  
 def LibraryDetailView(self, request):
-
+    """Injects additional context data specific to the book."""
+    # context = super().get_context_data(**kwargs)  # Get default context data
     context = {'detail_view': book}
-    book = self.get_object()
-
-    return render( request, 'relationship_app/library_detail.html', context)
-
+    book = self.get_object()# Retrieve the current book instance
+    return render(request, 'relationship_app/library_detail.html', context)
+    # context['average_rating'] = book.get_average_rating() 
+# Create your views here.
