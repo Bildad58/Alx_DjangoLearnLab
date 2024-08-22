b510a3edfab2bb0a524c998e38ff99508b9939a0
Bill . <bildadwasioya@gmail.com>
2024-08-22 10:07:54 +0300

    update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,17 +4,17 @@ from django.http import HttpResponse
 
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def my_view(request):
+def book_list(request):
     return HttpResponse
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
-def my_view(request):
+def book_list(request):
     return HttpResponse
 
 @permission_required('bookshelf.can_create', raise_exception=True)
-def my_view(request):
+def book_list(request):
     return HttpResponse
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def my_view(request):
+def book_list(request):
     return HttpResponse
