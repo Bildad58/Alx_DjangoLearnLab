569ea330f6e5087f34d4dee344e935bf06e596be
Bill . <bildadwasioya@gmail.com>
2024-09-12 11:20:30 +0300

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('posts/', PostListView.as_view() , name='post-list'),
     path('post/new/', PostCreateView.as_view(), name='create-post'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-detail'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
   
 
